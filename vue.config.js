83b062c2ec6a1c3986dcdc61ff87b1947ab726a3
Paul <alphagoo@g-mail.nsysu.edu.tw>
2022-01-26 21:57:02 +0800

    build: :construction_worker: update `baseUrl` in production build

--- vue.config.js
+++ vue.config.js
@@ -4,4 +4,5 @@ module.exports = {
       // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
     },
   },
+  baseUrl: process.env.NODE_ENV === "production" ? "/weddings/" : "/",
 };
